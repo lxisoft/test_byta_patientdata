0a026d768c99cb24fff6cd8c102e5b1b77293b3d
rafeek <Abdul.Rafeek@lxisoft.com>
2017-04-11 13:33:23 +0530

    unwanted files deleted

--- src/main/java/com/lxisoft/repository/PatientFamilyRepo.java
+++
@@ -1,9 +0,0 @@
-package com.lxisoft.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.lxisoft.entity.PatientFamilyRelation;
-
-public interface PatientFamilyRepo extends JpaRepository<PatientFamilyRelation,Integer> {
-
-}
