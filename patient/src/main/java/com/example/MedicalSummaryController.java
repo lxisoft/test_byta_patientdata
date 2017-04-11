78f727289762c42b7c00660f6a72169aaf123506
sreejith <sreejith.r@lxisoft.com>
2017-04-11 15:04:29 +0530

    resolved #8

---
+++ patient/src/main/java/com/example/MedicalSummaryController.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MedicalSummaryController {
+	@RequestMapping("/medicalSummary")
+	public String openPatientRecord() {
+		return "Medical summary completed";
+	}
+
+}
