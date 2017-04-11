eb0f2ae0ca3a68dcc72c14075ec149fbc5840639
sreejith <sreejith.r@lxisoft.com>
2017-04-11 15:06:03 +0530

    resolved #9

---
+++ patient/src/main/java/com/example/TraceController.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TraceController {
+	@RequestMapping("/trace")
+	public String openPatientRecord() {
+		return "Trace  completed";
+	}
+
+}
