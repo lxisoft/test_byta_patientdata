fed3dc85def916870836bd7fb52af33ad34b0630
basil <basil.jude@lxisoft.com>
2017-04-11 14:44:47 +0530

    fixed #5

--- patient/src/main/java/com/example/registrationController.java
+++ patient/src/main/java/com/example/registrationController.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class  registrationController {
-	@RequestMapping("/CurrentDetailRegistration")
+	@RequestMapping("/wildcardSearching")
 	public String CurrentDetailRegistration() {
-		return "CurrentDetailRegistration completed";
+		return "wildcardSearching completed";
 	}
 }
